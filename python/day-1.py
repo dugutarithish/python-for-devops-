7be845450b1fab0e70a90d28b0fc753318ac5cf8
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-14 23:08:47 +0530

    Update day-1.py

--- python/day-1.py
+++ python/day-1.py
@@ -1,12 +1,13 @@
 import boto3
 
-ec2 = boto3.client("ec2", region_name= "ap-south-1")
+ec2 = boto3.client("ec2", region_name="ap-south-1")
 
 response = ec2.describe_instances()
 
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
-        print("Instances ID:", instance["InstanceId"])
-        print("State:", instance["State"]["Name])
-        print("Instant_type:", instance["InstanceType"])
+        print("Instance ID:", instance["InstanceId"])
+        print("State:", instance["State"]["Name"])
+        print("Instance Type:", instance["InstanceType"])
+        print("Private IP:", instance.get("PrivateIpAddress", "N/A"))
         print("-" * 30)
